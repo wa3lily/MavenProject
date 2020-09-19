1b2b323c5e903349531b0e52f874ea4380d987b1
wa3lily <liik@mail.ru>
2020-09-20 02:10:12 +0300

    first p

--- src/main/java/ru/sfedu/mavenproject/Main.java
+++ src/main/java/ru/sfedu/mavenproject/Main.java
@@ -1,14 +1,16 @@
 package ru.sfedu.mavenproject;
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+
     private static Logger log = LogManager.getLogger(Main.class);
 
-    static public void main(){
+    static public void main(String[] args){
         log.info("Info_test");
         log.debug("Debag_test");
         log.error("Error_test");
+        log.info(Constants.TEST_CONST);
     }
 }
--- src/test/java/ru/sfedu/mavenproject/MainTest.java
+++ src/test/java/ru/sfedu/mavenproject/MainTest.java
@@ -1,7 +1,16 @@
 package ru.sfedu.mavenproject;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    @Test
+    void main() {
+        System.out.println("main");
+        System.out.println(Constants.TEST_CONST);
+        System.out.println("Good_test");
+        //fail("ddd");
+    }
 }
