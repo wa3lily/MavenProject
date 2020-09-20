ccdc36dc98035d4b7930e4b33c9fe8483e2b69c1
wa3lily <liik@mail.ru>
2020-09-21 00:22:23 +0300

    second p

--- src/main/java/ru/sfedu/mavenproject/Main.java
+++ src/main/java/ru/sfedu/mavenproject/Main.java
@@ -3,14 +3,20 @@ package ru.sfedu.mavenproject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+
+import static ru.sfedu.mavenproject.Constants.ENV_CONST;
+import static ru.sfedu.mavenproject.utils.ConfigurationUtil.getConfigurationEntry;
+
 public class Main {
 
     private static Logger log = LogManager.getLogger(Main.class);
 
-    static public void main(String[] args){
+    static public void main(String[] args) throws IOException {
         log.info("Info_test");
         log.debug("Debag_test");
         log.error("Error_test");
         log.info(Constants.TEST_CONST);
+        System.out.println(getConfigurationEntry(ENV_CONST));
     }
 }
