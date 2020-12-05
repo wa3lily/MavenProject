b8bc16a196395ab5339ac02b6a5078c905b19085
wa3lily <liik@mail.ru>
2020-12-06 00:20:47 +0300

    some trubble, inclue with relations

--- src/main/java/ru/sfedu/mavenproject/api/DataProviderCSV.java
+++ src/main/java/ru/sfedu/mavenproject/api/DataProviderCSV.java
@@ -13,11 +13,10 @@ import org.apache.logging.log4j.Logger;
 import ru.sfedu.mavenproject.ClassId;
 import ru.sfedu.mavenproject.bean.*;
 import ru.sfedu.mavenproject.utils.ConfigurationUtil;
-
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-
 import static ru.sfedu.mavenproject.Constants.PATH;
 import static ru.sfedu.mavenproject.Constants.FILE_EXTENSION;
 
@@ -25,12 +24,14 @@ public class DataProviderCSV {
 
     private static Logger log = LogManager.getLogger(DataProviderCSV.class);
 
+    //CRUD and helper methods
+
     public FileWriter commonWriter(Class cl) throws IOException {
         FileWriter writer = new FileWriter(getPath(cl));
         return writer;
     }
 
-    public <T> List<T> select(Class cl) throws IOException{
+    public <T> List<T> read(Class cl) throws IOException{
         FileReader fileReader;
         String path = getPath(cl);
         try{
@@ -55,85 +56,7 @@ public class DataProviderCSV {
     }
 
     public <T extends ClassId> Object getByID(Class cl, long id) throws IOException {
-        List<T> list = this.select(cl);
-        try {
-            Object obj = list.stream()
-                    .filter(e1 -> e1.getId() == id)
-                    .findFirst().get();
-            return obj;
-        }catch (NoSuchElementException e){
-            log.error(e);
-            return null;
-        }
-    }
-
-    public <T extends People> Object getPeopleByID(Class cl, long id) throws IOException {
-        List<T> list = this.select(cl);
-        try {
-            Object obj = list.stream()
-                    .filter(e1 -> e1.getId() == id)
-                    .findFirst().get();
-            return obj;
-        }catch (NoSuchElementException e){
-            log.error(e);
-            return null;
-        }
-    }
-
-    public <T extends Book> Object getBookByID(Class cl, long id) throws IOException {
-        List<T> list = this.select(cl);
-        try {
-            Object obj = list.stream()
-                    .filter(e1 -> e1.getId() == id)
-                    .findFirst().get();
-            return obj;
-        }catch (NoSuchElementException e){
-            log.error(e);
-            return null;
-        }
-    }
-
-    public <T extends Meeting> Object getMeetingByID(Class cl, long id) throws IOException {
-        List<T> list = this.select(cl);
-        try {
-            Object obj = list.stream()
-                    .filter(e1 -> e1.getId() == id)
-                    .findFirst().get();
-            return obj;
-        }catch (NoSuchElementException e){
-            log.error(e);
-            return null;
-        }
-    }
-
-    public <T extends Corrections> Object getCorrectionsByID(Class cl, long id) throws IOException {
-        List<T> list = this.select(cl);
-        try {
-            Object obj = list.stream()
-                    .filter(e1 -> e1.getId() == id)
-                    .findFirst().get();
-            return obj;
-        }catch (NoSuchElementException e){
-            log.error(e);
-            return null;
-        }
-    }
-
-    public <T extends CoverPrice> Object getCoverPriceByID(Class cl, long id) throws IOException {
-        List<T> list = this.select(cl);
-        try {
-            Object obj = list.stream()
-                    .filter(e1 -> e1.getId() == id)
-                    .findFirst().get();
-            return obj;
-        }catch (NoSuchElementException e){
-            log.error(e);
-            return null;
-        }
-    }
-
-    public <T extends PriceParameters> Object getPriceParametersByID(Class cl, long id) throws IOException {
-        List<T> list = this.select(cl);
+        List<T> list = this.read(cl);
         try {
             Object obj = list.stream()
                     .filter(e1 -> e1.getId() == id)
@@ -151,17 +74,133 @@ public class DataProviderCSV {
         return path;
     }
 
-    public <T> void insert(Class cl, List<T> list) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+    public <T extends ClassId> List<T> insertWithoutRelation(Class cl, List<T> list) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+        //список для элементов, нарушающих уникальность ключей
+        List<T> returnList = new ArrayList<T>();
+        //список для элементов для записи в csv
+        List<T> recordList = read(cl);
+        log.debug("csv: "+recordList);
+        for (T elem : list) {
+            if (recordList.stream().noneMatch(e1 -> e1.getId() == elem.getId())){
+                recordList.add(elem);
+                log.debug("add to csv "+elem);
+            }else if ((recordList.stream().anyMatch(e1 -> e1.getId() == elem.getId() && e1.equals(elem)))){
+                log.debug("already exist "+elem);
+            }else{
+                returnList.add(elem);
+                log.debug("wrong id "+elem);
+            }
+        }
         try {
             CSVWriter csvWriter = new CSVWriter(commonWriter(cl));
             StatefulBeanToCsv<T> beanToCsv = new StatefulBeanToCsvBuilder<T>(csvWriter)
                     .withApplyQuotesToAll(false)
                     .build();
-            beanToCsv.write(list);
+            beanToCsv.write(recordList);
             csvWriter.close();
         }catch (IndexOutOfBoundsException e){
             log.error(e);
         }
+        //возвращает список элементов, которые не удалось записать
+        return returnList;
     }
+
+    public <T extends ClassId> List<T> insert(Class cl, List<T> list) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+        List<T> returnList = new ArrayList<T>();
+        List<T> recordList = new ArrayList<>();
+        switch (cl.getSimpleName().toLowerCase()){
+            //classes without relation to other classes
+            case "people":
+            case "author":
+            case "employee":
+            case "meeting":
+            case "coverprice":
+                returnList.addAll(insertWithoutRelation(cl, list));
+                break;
+                //classes with relation to other classes
+            case "book":
+                List<Book> bookList = (List<Book>) list;
+                for (Book elem : bookList) {
+                    if (getByID(Author.class, elem.getAuthor().getId()) != null){
+                        recordList.add((T)elem);
+                        log.debug("there is such Author in csv");
+                    }else{
+                        returnList.add((T)elem);
+                        log.debug("there is no such Author in csv");
+                    }
+                }
+                returnList.addAll(insertWithoutRelation(cl, recordList));
+                break;
+            case "priceparameters":
+                List<PriceParameters> priceList = (List<PriceParameters>) list;
+                for (PriceParameters elem : priceList) {
+                    if (getByID(CoverPrice.class, elem.getCoverPrice().getId()) != null){
+                        recordList.add((T)elem);
+                        log.debug("there is such CoverPrice in csv");
+                    }else{
+                        returnList.add((T)elem);
+                        log.debug("there is no such CoverPrice in csv");
+                    }
+                }
+                returnList.addAll(insertWithoutRelation(cl, recordList));
+                break;
+            case "corrections":
+                List<Corrections> correctionsList = (List<Corrections>) list;
+                for (Corrections elem : correctionsList) {
+                    if (getByID(Order.class, elem.getOrder().getId()) != null && (elem.getMeet() == null || getByID(Meeting.class, elem.getMeet().getId()) != null)){
+                        recordList.add((T)elem);
+                        log.debug("there is such Order and Meeting in csv");
+                    }else{
+                        returnList.add((T)elem);
+                        log.debug("there is no such Order or Meeting in csv");
+                    }
+                }
+                returnList.addAll(insertWithoutRelation(cl, recordList));
+                break;
+            case "order":
+                List<Order> orderList = (List<Order>) list;
+                for (Order elem : orderList) {
+                    if ((elem.getBookPriceParameters() == null || getByID(PriceParameters.class, elem.getBookPriceParameters().getId()) != null)
+                            && (elem.getBookMaker() == null || getByID(Employee.class, elem.getBookMaker().getId()) != null)
+                            && (elem.getBookEditor() == null || getByID(Employee.class, elem.getBookEditor().getId()) != null)){
+                        recordList.add((T)elem);
+                        log.debug("there is such PriceParameters and Employee in csv");
+                    }else{
+                        returnList.add((T)elem);
+                        log.debug("there is no such PriceParameters or Employee in csv");
+                    }
+                }
+                returnList.addAll(insertWithoutRelation(cl, recordList));
+                break;
+            default:
+                log.debug("default case");
+        }
+        return returnList;
+    }
+
+    public boolean deleteFile(Class cl) {
+        try{
+            File file= new File(getPath(cl));
+            log.debug("file delete "+file.delete());
+            return true;
+        }catch (IOException e){
+            log.error(e);
+            return false;
+        }
+    }
+
+    public <T extends ClassId> List<Long> readId(Class cl) throws IOException {
+        List<T> list = read(cl);
+        List<Long> result = new ArrayList<>();
+        for (T elem : list) {
+            result.add(elem.getId());
+        }
+        return result;
+    }
+
+
+
+
+
 
 }
--- src/test/java/ru/sfedu/mavenproject/api/DataProviderCSVTest.java
+++ src/test/java/ru/sfedu/mavenproject/api/DataProviderCSVTest.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+import ru.sfedu.mavenproject.ClassId;
 import ru.sfedu.mavenproject.TestBase;
 import ru.sfedu.mavenproject.bean.*;
 import ru.sfedu.mavenproject.enums.BookStatus;
@@ -11,7 +12,10 @@ import ru.sfedu.mavenproject.enums.CorrectionsStatus;
 import ru.sfedu.mavenproject.enums.CoverType;
 import ru.sfedu.mavenproject.enums.EmployeeType;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,6 +36,7 @@ class DataProviderCSVTest extends TestBase {
         listPeople.add(people1);
         listPeople.add(people2);
         listPeople.add(people3);
+        instance.deleteFile(People.class);
         instance.insert(People.class, listPeople);
         assertEquals(people2, instance.getByID(People.class, 2));
     }
@@ -47,6 +52,7 @@ class DataProviderCSVTest extends TestBase {
         listPeople.add(people1);
         listPeople.add(people2);
         listPeople.add(people3);
+        instance.deleteFile(People.class);
         instance.insert(People.class, listPeople);
         assertNull(instance.getByID(People.class, 4));
     }
@@ -58,6 +64,7 @@ class DataProviderCSVTest extends TestBase {
         DataProviderCSV instance = new DataProviderCSV();
         Author author = createAuthor(1,"Виктор","Иванович","Ткач","83456789012", "tkach@gmail.com", "docent", "Donstu");
         listAuthor.add(author);
+        instance.deleteFile(Author.class);
         instance.insert(Author.class, listAuthor);
         assertEquals(author, instance.getByID(Author.class, 1));
     }
@@ -69,6 +76,7 @@ class DataProviderCSVTest extends TestBase {
         DataProviderCSV instance = new DataProviderCSV();
         Author author = createAuthor(1,"Виктор","Иванович","Ткач","83456789012", "tkach@gmail.com", "docent", "Donstu");
         listAuthor.add(author);
+        instance.deleteFile(Author.class);
         instance.insert(Author.class, listAuthor);
         assertNull(instance.getByID(Author.class, 2));
     }
@@ -84,6 +92,7 @@ class DataProviderCSVTest extends TestBase {
         listEmployee.add(employee1);
         listEmployee.add(employee2);
         listEmployee.add(employee3);
+        instance.deleteFile(Employee.class);
         instance.insert(Employee.class, listEmployee);
         assertEquals(employee2, instance.getByID(Employee.class, 2));
     }
@@ -99,6 +108,7 @@ class DataProviderCSVTest extends TestBase {
         listEmployee.add(employee1);
         listEmployee.add(employee2);
         listEmployee.add(employee3);
+        instance.deleteFile(Employee.class);
         instance.insert(Employee.class, listEmployee);
         assertNull(instance.getByID(Employee.class, 4));
     }
@@ -110,6 +120,7 @@ class DataProviderCSVTest extends TestBase {
         DataProviderCSV instance = new DataProviderCSV();
         Meeting meeting = createMeeting(1,"15.12.2020 11:55",true, false);
         listMeeting.add(meeting);
+        instance.deleteFile(Meeting.class);
         instance.insert(Meeting.class, listMeeting);
         assertEquals(meeting, instance.getByID(Meeting.class, 1));
     }
@@ -121,6 +132,7 @@ class DataProviderCSVTest extends TestBase {
         DataProviderCSV instance = new DataProviderCSV();
         Meeting meeting = createMeeting(1,"15.12.2020 11:55",true, false);
         listMeeting.add(meeting);
+        instance.deleteFile(Meeting.class);
         instance.insert(Meeting.class, listMeeting);
         assertNull(instance.getByID(Meeting.class, 2));
     }
@@ -132,6 +144,7 @@ class DataProviderCSVTest extends TestBase {
         DataProviderCSV instance = new DataProviderCSV();
         CoverPrice coverPrice = createCoverPrice(1,CoverType.RIGID_COVER, 123.5);
         listCoverPrice.add(coverPrice);
+        instance.deleteFile(CoverPrice.class);
         instance.insert(CoverPrice.class, listCoverPrice);
         assertEquals(coverPrice, instance.getByID(CoverPrice.class, 1));
     }
@@ -143,6 +156,7 @@ class DataProviderCSVTest extends TestBase {
         DataProviderCSV instance = new DataProviderCSV();
         CoverPrice coverPrice = createCoverPrice(1,CoverType.RIGID_COVER, 123.5);
         listCoverPrice.add(coverPrice);
+        instance.deleteFile(CoverPrice.class);
         instance.insert(CoverPrice.class, listCoverPrice);
         assertNull(instance.getByID(CoverPrice.class, 2));
     }
@@ -151,10 +165,15 @@ class DataProviderCSVTest extends TestBase {
     public void testInsertPriceParametersSuccess() throws Exception{
         log.info("insertPriceParametersSuccess");
         List<PriceParameters> listPriceParameters = new ArrayList<>();
+        List<CoverPrice> listCoverPrice = new ArrayList<>();
         DataProviderCSV instance = new DataProviderCSV();
         CoverPrice coverPrice = createCoverPrice(1,CoverType.RIGID_COVER, 123.5);
         PriceParameters priceParameters = createPriceParameters(1, 13.4, coverPrice, 16.3, "01.01.2019", "01.01.2021");
         listPriceParameters.add(priceParameters);
+        listCoverPrice.add(coverPrice);
+        instance.deleteFile(PriceParameters.class);
+        instance.deleteFile(CoverPrice.class);
+        instance.insert(CoverPrice.class, listCoverPrice);
         instance.insert(PriceParameters.class, listPriceParameters);
         assertEquals(priceParameters, instance.getByID(PriceParameters.class, 1));
     }
@@ -167,14 +186,20 @@ class DataProviderCSVTest extends TestBase {
         CoverPrice coverPrice = createCoverPrice(1,CoverType.RIGID_COVER, 123.5);
         PriceParameters priceParameters = createPriceParameters(1, 13.4, coverPrice, 16.3, "01.01.2019", "01.01.2021");
         listPriceParameters.add(priceParameters);
+        log.debug(instance.deleteFile(CoverPrice.class));
+        log.debug(instance.deleteFile(PriceParameters.class));
         instance.insert(PriceParameters.class, listPriceParameters);
-        assertNull(instance.getByID(PriceParameters.class, 2));
+        log.debug(instance.getByID(PriceParameters.class, 1));
+        assertNull(instance.getByID(PriceParameters.class, 1));
     }
 
     @Test
     public void testInsertOrderSuccess() throws Exception{
         log.info("insertOrderSuccess");
         List<Order> listOrder = new ArrayList<>();
+        List<Employee> listEmployee = new ArrayList<>();
+        List<CoverPrice> listCoverPrice = new ArrayList<>();
+        List<PriceParameters> listPriceParameters = new ArrayList<>();
         DataProviderCSV instance = new DataProviderCSV();
         Employee employee2 = createEmployee(2,"Петр","Петрович","Петров","82345678901","234567890123", "2345678", EmployeeType.MAKER);
         Employee employee3 = createEmployee(3,"Виктор","Иванович","Ткач","83456789012", "345678901234", "3456789", EmployeeType.EDITOR);
@@ -183,6 +208,17 @@ class DataProviderCSVTest extends TestBase {
         PriceParameters priceParameters = createPriceParameters(1, 2.4, coverPrice, 1.3, "01.01.2019", "01.01.2021");
         Order order = createOrder(1,author,"Цифровая бухгалтерия",4,"03.09.2020", CoverType.RIGID_COVER, employee2, employee3, priceParameters, 229, 100, 9700.75 , BookStatus.EDITING);
         listOrder.add(order);
+        listEmployee.add(employee2);
+        listEmployee.add(employee3);
+        listCoverPrice.add(coverPrice);
+        listPriceParameters.add(priceParameters);
+        instance.deleteFile(Order.class);
+        instance.deleteFile(Employee.class);
+        instance.deleteFile(CoverPrice.class);
+        instance.deleteFile(PriceParameters.class);
+        instance.insert(Employee.class, listEmployee);
+        instance.insert(CoverPrice.class, listCoverPrice);
+        instance.insert(PriceParameters.class, listPriceParameters);
         instance.insert(Order.class, listOrder);
         assertEquals(order, instance.getByID(Order.class, 1));
     }
@@ -199,14 +235,23 @@ class DataProviderCSVTest extends TestBase {
         PriceParameters priceParameters = createPriceParameters(1, 2.4, coverPrice, 1.3, "01.01.2019", "01.01.2021");
         Order order = createOrder(1,author,"Цифровая бухгалтерия",4,"03.09.2020", CoverType.RIGID_COVER, employee2, employee3, priceParameters, 229, 100, 9700.75 , BookStatus.EDITING);
         listOrder.add(order);
+        instance.deleteFile(Employee.class);
+        instance.deleteFile(CoverPrice.class);
+        instance.deleteFile(PriceParameters.class);
+        instance.deleteFile(Order.class);
         instance.insert(Order.class, listOrder);
-        assertNull(instance.getByID(Order.class, 2));
+        assertNull(instance.getByID(Order.class, 1));
     }
 
     @Test
     public void testInsertCorrectionsSuccess() throws Exception{
         log.info("insertCorrectionsSuccess");
         List<Corrections> listCorrections = new ArrayList<>();
+        List<Employee> listEmployee = new ArrayList<>();
+        List<Author> listAuthor = new ArrayList<>();
+        List<CoverPrice> listCoverPrice = new ArrayList<>();
+        List<PriceParameters> listPriceParameters = new ArrayList<>();
+        List<Order> listOrder = new ArrayList<>();
         DataProviderCSV instance = new DataProviderCSV();
         Employee employee2 = createEmployee(2,"Петр","Петрович","Петров","82345678901","234567890123", "2345678", EmployeeType.MAKER);
         Employee employee3 = createEmployee(3,"Виктор","Иванович","Ткач","83456789012", "345678901234", "3456789", EmployeeType.EDITOR);
@@ -216,7 +261,23 @@ class DataProviderCSVTest extends TestBase {
         Order order = createOrder(1,author,"Цифровая бухгалтерия",4,"03.09.2020", CoverType.RIGID_COVER, employee2, employee3, priceParameters, 229, 100, 9700.75 , BookStatus.EDITING  );
         Corrections corrections = createCorrections(1,35, "Цифровой контроль - это компьютерные системы",
                 "Цифровой контроль представляет собой компьютерные системы", "Повторяется конструкция", order, null, CorrectionsStatus.WAIT_AUTHOR_AGR );
+        listEmployee.add(employee2);
+        listEmployee.add(employee3);
+        listAuthor.add(author);
+        listCoverPrice.add(coverPrice);
+        listPriceParameters.add(priceParameters);
+        listOrder.add(order);
         listCorrections.add(corrections);
+        instance.deleteFile(Employee.class);
+        instance.deleteFile(Author.class);
+        instance.deleteFile(PriceParameters.class);
+        instance.deleteFile(Order.class);
+        instance.deleteFile(Corrections.class);
+        instance.insert(Employee.class, listEmployee);
+        instance.insert(Author.class, listAuthor);
+        instance.insert(CoverPrice.class, listCoverPrice);
+        instance.insert(PriceParameters.class, listPriceParameters);
+        instance.insert(Order.class, listOrder);
         instance.insert(Corrections.class, listCorrections);
         assertEquals(corrections, instance.getByID(Corrections.class, 1));
     }
@@ -235,21 +296,30 @@ class DataProviderCSVTest extends TestBase {
         Corrections corrections = createCorrections(1,35, "Цифровой контроль - это компьютерные системы",
                 "Цифровой контроль представляет собой компьютерные системы", "Повторяется конструкция", order, null, CorrectionsStatus.WAIT_AUTHOR_AGR );
         listCorrections.add(corrections);
+        instance.deleteFile(Employee.class);
+        instance.deleteFile(Author.class);
+        instance.deleteFile(PriceParameters.class);
+        instance.deleteFile(Order.class);
+        instance.deleteFile(Corrections.class);
         instance.insert(Corrections.class, listCorrections);
-        assertNull(instance.getByID(Corrections.class, 2));
+        assertNull(instance.getByID(Corrections.class, 1));
     }
 
     @Test
     public void testInsertBookSuccess() throws Exception{
         log.info("insertBookSuccess");
         List<Book> listBook = new ArrayList<>();
+        List<Author> listAuthor = new ArrayList<>();
         DataProviderCSV instance = new DataProviderCSV();
         Author author = createAuthor(10,"Виктор","Иванович","Ткач","83456789012", "tkach@gmail.com", "docent", "Donstu");
         Book book = createBook(1,author,"Цифровая бухгалтерия",4);
         listBook.add(book);
+        listAuthor.add(author);
+        instance.deleteFile(Author.class);
+        instance.deleteFile(Book.class);
+        instance.insert(Author.class, listAuthor);
         instance.insert(Book.class, listBook);
         assertEquals(book, instance.getByID(Book.class,1));
-
     }
 
     @Test
@@ -260,8 +330,84 @@ class DataProviderCSVTest extends TestBase {
         Author author = createAuthor(10,"Виктор","Иванович","Ткач","83456789012", "tkach@gmail.com", "docent", "Donstu");
         Book book = createBook(1,author,"Цифровая бухгалтерия",4);
         listBook.add(book);
+        instance.deleteFile(Author.class);
+        instance.deleteFile(Book.class);
         instance.insert(Book.class, listBook);
-        assertNull(instance.getByID(Book.class,2));
-
+        assertNull(instance.getByID(Book.class,1));
     }
+
+    @Test
+    public void testInsertSame() throws Exception{
+        log.info("testInsertSame");
+        List<Employee> listEmployee = new ArrayList<>();
+        List<Employee> expectListEmployee = new ArrayList<>();
+        DataProviderCSV instance = new DataProviderCSV();
+        instance.deleteFile(Employee.class);
+        Employee employee1 = createEmployee(1,"Иван","Иванович","Иванов","81234567890", "123456789012","1234567", EmployeeType.CHIEF);
+        Employee employee2 = createEmployee(2,"Петр","Петрович","Петров","82345678901","234567890123", "2345678", EmployeeType.MAKER);
+        Employee employee3 = createEmployee(2,"Виктор","Иванович","Ткач","83456789012", "345678901234", "3456789", EmployeeType.EDITOR);
+        listEmployee.add(employee1);
+        listEmployee.add(employee2);
+        instance.insert(Employee.class, listEmployee);
+        listEmployee.clear();
+        listEmployee.add(employee1);
+        listEmployee.add(employee3);
+        expectListEmployee.add(employee3);
+        assertEquals(expectListEmployee, instance.insert(Employee.class, listEmployee));
+        expectListEmployee.clear();
+        expectListEmployee.add(employee1);
+        expectListEmployee.add(employee2);
+        assertEquals(expectListEmployee, instance.read(Employee.class));
+    }
+
+    //read
+    @Test
+    public void testReadSuccess() throws Exception{
+        log.info("testReadSuccess");
+        List<Long> listId = new ArrayList<>(Arrays.asList((long)1,(long)2,(long)3));
+        List<Employee> listEmployee = new ArrayList<>();
+        DataProviderCSV instance = new DataProviderCSV();
+        Employee employee1 = createEmployee(1,"Иван","Иванович","Иванов","81234567890", "123456789012","1234567", EmployeeType.CHIEF);
+        Employee employee2 = createEmployee(2,"Петр","Петрович","Петров","82345678901","234567890123", "2345678", EmployeeType.MAKER);
+        Employee employee3 = createEmployee(3,"Виктор","Иванович","Ткач","83456789012", "345678901234", "3456789", EmployeeType.EDITOR);
+        listEmployee.add(employee1);
+        listEmployee.add(employee2);
+        listEmployee.add(employee3);
+        instance.deleteFile(Employee.class);
+        instance.insert(Employee.class, listEmployee);
+        assertEquals(listId, instance.readId(Employee.class));
+    }
+
+    @Test
+    public void testReadFail() throws Exception{
+        log.info("testReadFail");
+        List<Long> listId = new ArrayList<>();
+        DataProviderCSV instance = new DataProviderCSV();
+        instance.deleteFile(Employee.class);
+        assertEquals(listId, instance.read(Employee.class));
+    }
+
+    //update
+    /*
+    @Test
+    public void testUpdateSuccess() throws Exception{
+        log.info("testUpdateSuccess");
+        List<Employee> listEmployee = new ArrayList<>();
+        DataProviderCSV instance = new DataProviderCSV();
+        Employee employee1 = createEmployee(1,"Иван","Иванович","Иванов","81234567890", "123456789012","1234567", EmployeeType.CHIEF);
+        Employee employee2 = createEmployee(2,"Петр","Петрович","Петров","82345678901","234567890123", "2345678", EmployeeType.MAKER);
+        Employee employee3 = createEmployee(2,"Виктор","Иванович","Ткач","83456789012", "345678901234", "3456789", EmployeeType.EDITOR);
+        listEmployee.add(employee1);
+        listEmployee.add(employee2);
+        instance.deleteFile(Employee.class);
+        instance.insert(Employee.class, listEmployee);
+        instance.update(Employee.class, employee3);
+        listEmployee.clear();
+        listEmployee.add(employee1);
+        listEmployee.add(employee3);
+        assertEquals(listEmployee, instance.read(Employee.class));
+    }
+
+     */
+
 }
